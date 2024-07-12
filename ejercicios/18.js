3a53a295833854352fc11f43c383b122b76f9240
NRodriguez <n87420-ind@outlook.com>
2024-07-11 22:53:09 -0300

    ejercicios c5 completos

---
+++ ejercicios/18.js
@@ -0,0 +1,23 @@
+function esVocal(letra) {
+  // La función recibe un argumento llamado letra el cual es un string.
+  // Retorna como resultado un string que indica si el argumento letra es una vocal.
+  // de lo contrario retorna el string "Dato incorrecto"
+  // Por ejemplo: 
+  // "a" ---> "Es vocal"
+  // "u" ---> "Es vocal"
+  // "n" ---> "Dato incorrecto"
+  // "texto largo" ---> "Dato incorrecto"
+  // Tu código:
+  if (typeof letra === "string" && letra.length === 1) {
+    const vocales = "aeiouAEIOU";
+    if (vocales.includes(letra)) {
+      return "Es vocal"
+    } else {
+      return "Dato incorrecto"
+    }
+  } else {
+    return "Dato incorrecto"
+  }
+}
+
+module.exports = esVocal;
